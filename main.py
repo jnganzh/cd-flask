792fd1b982b89a08f049ba757eb71f1ff4010960
jaryl <jarylngan95@gmail.com>
2021-02-06 14:11:17 +0000

    changed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
+    return 'Hello I like to make AI Apps Burp'
 
 @app.route('/name/<value>')
 def name(value):
