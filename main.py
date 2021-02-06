a1820212976fe10c048f22c49f99cfab11c5056e
jaryl <jarylngan95@gmail.com>
2021-02-06 15:21:25 +0000

    crypto added

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ def wikipedia_route(company):
     entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
     return str(entities)
 
+@app.route('/crypto')
+def crypto():
+    return redirect('https://www.coingecko.com/')
 
 
 
